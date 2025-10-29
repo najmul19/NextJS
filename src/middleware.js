0694f0471b36d2cd949a261570e0a9ff98c9e119
najmul <mdnajmulislam10992@gmail.com>
2025-10-29 23:09:26 +0600

    protected route

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,22 @@
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request) {
-    const dummyUserData = {
-        role: "admin",
-        email: "test@admin.com"
-    }
-  let isServicesPage = request.nextUrl.pathname.startsWith("/services");
-  // console.log("PATHNAME: ",request.nextUrl.pathname.startsWith("/services"))
-  let isAdmin = dummyUserData.role=="admin"
-  if(isServicesPage && !isAdmin){
-    return NextResponse.redirect(new URL("/login", request.url))
+export async function  middleware(req) {
+  const token = await getToken({req});
+  // if(token){
+  //   console.log("form midlware: ",token)
+  // }
+  const isTokenOk = Boolean(token);
+  const isAdminUser = token?.role=="admin"
+  const isAdminSpecificRoute = req.nextUrl.pathname.startsWith("/dashboared")
+  if(isAdminSpecificRoute && !isAdminUser) {
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url))
   }
-  return NextResponse.next(); // client request will come in to midlware andd without any checking its through next step
+  
+  return NextResponse.next(); // client req will come in to midlware and without any checking its through next step
 }
 
- 
 // See "Matching Paths" below to learn more
 // export const config = {
 //   matcher: '/about/:path*',
