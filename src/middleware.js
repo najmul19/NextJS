c6d1e96c4589181a721b0bcad2bc6e66f95339e4
najmul <mdnajmulislam10992@gmail.com>
2025-10-29 23:15:03 +0600

    redirect user

--- src/middleware.js
+++ src/middleware.js
@@ -2,18 +2,21 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export async function  middleware(req) {
-  const token = await getToken({req});
+export async function middleware(req) {
+  const token = await getToken({ req });
   // if(token){
   //   console.log("form midlware: ",token)
   // }
   const isTokenOk = Boolean(token);
-  const isAdminUser = token?.role=="admin"
-  const isAdminSpecificRoute = req.nextUrl.pathname.startsWith("/dashboared")
-  if(isAdminSpecificRoute && !isAdminUser) {
-    return NextResponse.redirect(new URL("/api/auth/signin", req.url))
+  const isAdminUser = token?.role == "admin";
+  const isAdminSpecificRoute = req.nextUrl.pathname.startsWith("/dashboared");
+  if (isAdminSpecificRoute && !isAdminUser) {
+    const callbackUrl = encodeURIComponent(req.nextUrl.pathname);
+    return NextResponse.redirect(
+      new URL(`/api/auth/signin?callbackUrl=${callbackUrl}`, req.url)
+    );
   }
-  
+
   return NextResponse.next(); // client req will come in to midlware and without any checking its through next step
 }
 
